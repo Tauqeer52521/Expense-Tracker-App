f9546768842fa86fd3bcf4e68cdde29f25e78be2
Md Tauqeer <mdtauqeer525217@gmail.com>
2024-03-29 13:53:21 +0530

    creating register schema and set up backend for register and login and creating react file

--- Backend/app.js
+++ Backend/app.js
@@ -5,7 +5,7 @@ const cors=require('cors');
 const {readdirSync}=require('fs');
 
 const app=express();
-const port=process.env.PORT;
+const port=process.env.PORT||5000;
 
 //mongoose connection
 db();
